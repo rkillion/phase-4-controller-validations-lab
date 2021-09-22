78c17ad45d00704ec7acd92b0e8502548bc14e28
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-22 08:56:49 -0400

    commit tree 21ce17620a53dc4a952f8290b64bfddc809e3865

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+
+    private
+
+    def render_invalid_record(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
-  
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+
   def show
     author = Author.find(params[:id])
 
@@ -7,8 +8,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -18,4 +18,7 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  # def render_invalid_record(invalid)
+  #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  # end
 end
